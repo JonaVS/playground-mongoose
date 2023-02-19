0d35865e60cbd1cb9540229ac0734e3a3dff6366
JonaVS <jon4963@gmail.com>
2023-02-19 12:42:41 -0600

    style: format code at todoController

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -2,6 +2,6 @@ import { CreateTodoDTO, TodoDTO } from "../dtos/todo/todoDtos.js";
 import { Result } from "../types/Result.js";
 import * as todoService from "../db/services/todoService.js"
 
-export const createTodo = async (payload: CreateTodoDTO):Promise<Result<TodoDTO | null>> => {
-    return await todoService.createTodo(payload);
+export const createTodo = async (payload: CreateTodoDTO): Promise<Result<TodoDTO | null>> => {
+  return await todoService.createTodo(payload);
 };

37521c1ca1857d519c5d57cb3a2dd32c683ec615
JonaVS <jon4963@gmail.com>
2023-02-19 12:22:17 -0600

    style: format code at todoService

--- src/db/services/todoService.ts
+++ src/db/services/todoService.ts
@@ -5,12 +5,12 @@ import { toTodoDto } from "../../dtos/todo/todoDtoMappers.js";
 
 
 export const createTodo = async (payload: CreateTodoDTO): Promise<Result<TodoDTO | null>> => {
-    const dbResult = await todoDal.createTodo(payload);
+  const dbResult = await todoDal.createTodo(payload);
 
-    const result:Result<TodoDTO | null> = {
-        ...dbResult,
-        data: dbResult.data ? toTodoDto(dbResult.data) : null
-    }
+  const result: Result<TodoDTO | null> = {
+    ...dbResult,
+    data: dbResult.data ? toTodoDto(dbResult.data) : null,
+  };
 
-    return result
-} 
+  return result;
+};

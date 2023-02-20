a4999ad393641e6fa05fe74e74beb0f6fa6416b3
JonaVS <jon4963@gmail.com>
2023-02-20 15:48:22 -0600

    style: format code at todoDtos file

--- src/dtos/todo/todoDtos.ts
+++ src/dtos/todo/todoDtos.ts
@@ -2,5 +2,5 @@ import { ITodo } from "../../db/models/Todo.js";
 
 export type CreateTodoDTO = Pick<ITodo, 'title' | "description" | "completed">;
 export type TodoDTO = ITodo & {
-    id: string
+  id: string;
 };

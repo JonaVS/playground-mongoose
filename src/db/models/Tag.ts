2f29970b6d274067073e3ac6bf774ab22e998a71
JonaVS <jon4963@gmail.com>
2023-02-25 15:09:07 -0600

    refactor: rename tag schema declaration
    -Was using todoSchema instead of tagSchema

--- src/db/models/Tag.ts
+++ src/db/models/Tag.ts
@@ -6,11 +6,11 @@ export interface ITag {
   updatedAt: Date
 }
 
-const todoSchema = new Schema<ITag>(
+const tagSchema = new Schema<ITag>(
   {
     name: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-export const Tag = mongoose.model("Tag", todoSchema);
+export const Tag = mongoose.model("Tag", tagSchema);
